a1514a9a026ad976756746ff9c76fcbf0c0f9243
Marcus-Rise <ilykonst95@gmail.com>
2021-03-28 14:40:20 +0400

    Status meta styles

--- src/server-status/components/card/server-status-card.component.tsx
+++ src/server-status/components/card/server-status-card.component.tsx
@@ -1,18 +1,21 @@
 import type {FC} from 'react';
-import {ServerStatusModel} from "../../model";
+import {ServerStatusEnum, ServerStatusModel} from "../../model";
 import {Card, Centered} from "../../../components";
 import styles from "./server-status-card.module.scss"
+import classNames from "classnames";
 
 const ServerStatusCard: FC<{ status: ServerStatusModel }> = ({status}) => {
-  const items = Object.keys(status).map(i => (<li key={i}>
-    <b>{i}:</b> {status[i]}
-  </li>));
-
   return (
     <Card>
       <Centered>
         <ul className={styles.ul}>
-          {items}
+            <li><b>Name:</b> {status.name}</li>
+            <li className={classNames({
+              [styles.success]: status.status === ServerStatusEnum.ONLINE,
+              [styles.danger]: status.status === ServerStatusEnum.OFFLINE,
+            })}><b>Status:</b> {status.status}</li>
+            <li><b>Players:</b> {status.players > 0 ? status.players : "none"}</li>
+            <li><b>Version:</b> {status.version}</li>
         </ul>
       </Centered>
     </Card>
