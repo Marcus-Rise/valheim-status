e9c9febb8748dd74d38a02ab7b7e585e6f033de8
Marcus-Rise <ilykonst95@gmail.com>
2021-03-28 15:24:48 +0400

    Status card as table

--- src/server-status/components/card/server-status-card.component.tsx
+++ src/server-status/components/card/server-status-card.component.tsx
@@ -1,22 +1,46 @@
 import type {FC} from 'react';
+import {useMemo} from "react";
 import {ServerStatusEnum, ServerStatusModel} from "../../model";
 import {Card, Centered} from "../../../components";
 import styles from "./server-status-card.module.scss"
 import classNames from "classnames";
 
 const ServerStatusCard: FC<{ status: ServerStatusModel }> = ({status}) => {
+  const rowsObject: Record<string, { text: string | number, className?: string }> = useMemo(() => ({
+    Name: {
+      text: status.name
+    },
+    Status: {
+      text: status.status,
+      className: status.status === ServerStatusEnum.ONLINE ? styles.success : styles.danger
+    },
+    Players: {
+      text: status.players > 0 ? status.players : "none",
+    },
+    Version: {
+      text: status.version,
+    }
+  }), [status]);
+
+  const rows = useMemo(() => Object.keys(rowsObject).map(i => {
+    const row = rowsObject[i];
+
+    return (
+      <tr key={i}>
+        <td className={classNames(styles.title, styles.column)}>{i}</td>
+        <td className={classNames(row.className, styles.column)}>{row.text}</td>
+      </tr>
+    );
+  }), [rowsObject])
+
   return (
     <Card>
       <Centered>
-        <ul className={styles.ul}>
-            <li><b>Name:</b> {status.name}</li>
-            <li className={classNames({
-              [styles.success]: status.status === ServerStatusEnum.ONLINE,
-              [styles.danger]: status.status === ServerStatusEnum.OFFLINE,
-            })}><b>Status:</b> {status.status}</li>
-            <li><b>Players:</b> {status.players > 0 ? status.players : "none"}</li>
-            <li><b>Version:</b> {status.version}</li>
-        </ul>
+        <table>
+          <tbody>
+          {rows}
+          </tbody>
+        </table>
       </Centered>
     </Card>
   );
